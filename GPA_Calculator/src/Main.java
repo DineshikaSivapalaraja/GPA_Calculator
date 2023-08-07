386db3ed0ebab0ab8d06695aa6d25b77b21282f6
Dineshika <91914577+DineshikaSivapalaraja@users.noreply.github.com>
2023-08-07 14:43:49 +0530

    steps finished

--- GPA_Calculator/src/Main.java
+++ GPA_Calculator/src/Main.java
@@ -7,76 +7,96 @@ public class Main {
 		System.out.println("Welcome to GPA Calculator!!!\n");
 		
 		Scanner scanner = new Scanner(System.in);
+		Double totalPoints = 0.0;
+		Integer totalCredits = 0;
 		
-		boolean validCredits = true;//false;
-		Integer credits = 0;
+		boolean furtherCourses = false;
+		
 		do {
+		
+			boolean validCredits = true;//false;
+			Integer credits = 0;
+			do {
+				
+				 validCredits = true;
+				 
+			     System.out.print("Enter number of credits: ");  //println-> credit will show in next line
+			     String creditsString = scanner.nextLine();
+			     
+			     try {
+				      credits = Integer.parseInt(creditsString);
+			     }catch (NumberFormatException nfe) {
+				       System.out.println("Enter Valid Integer\n");
+				       validCredits = false;
+				 }
+			}
+			while (!validCredits);//(validCredits == false);
 			
-			 validCredits = true;
-			 
-		     System.out.print("Enter number of credits: ");  //println-> credit will show in next line
-		     String creditsString = scanner.nextLine();
-		     
-		     try {
-			      credits = Integer.parseInt(creditsString);
-		     }catch (NumberFormatException nfe) {
-			       System.out.println("Enter Valid Integer\n");
-			       validCredits = false;
-			 }
+			
+			boolean validGrade = true;
+	//		System.out.print("Enter grade            : ");
+	//		String grade = scanner.nextLine();
+			String grade = "";
+			Double gradeValue = 0.0;
+			
+			do {
+			  validGrade = true;
+			  System.out.print("Enter grade: ");
+	          grade = scanner.nextLine();
+			  
+			  if (grade.equalsIgnoreCase("A+") || (grade.equalsIgnoreCase("A"))) { //.equalsIgnoreCase-> for allow A,a without case sensitive
+				 gradeValue = 4.0;
+			  }else if (grade.equalsIgnoreCase("A-")) {
+				 gradeValue = 3.7;
+			  }else if (grade.equalsIgnoreCase("B+")) {
+				 gradeValue = 3.3;
+			  }else if (grade.equalsIgnoreCase("B")) {
+				 gradeValue = 3.0;
+			  }else if (grade.equalsIgnoreCase("B-")) {
+				 gradeValue = 2.7;
+			  }else if (grade.equalsIgnoreCase("C+")) {
+				 gradeValue = 2.4;
+			  }else if (grade.equalsIgnoreCase("C")) {
+				 gradeValue = 2.0;
+			  }else if (grade.equalsIgnoreCase("C-")) {
+				 gradeValue = 1.7;
+			  }else if (grade.equalsIgnoreCase("D+")) {
+				 gradeValue = 1.3;
+			  }else if (grade.equalsIgnoreCase("D")) {
+				 gradeValue = 1.0;
+			  }else if (grade.equalsIgnoreCase("E")) {
+				 gradeValue = 0.0;
+			  }else {
+				System.out.println("Grade is not Valid\n");		
+			    validGrade = false;
+			  }
+			}
+			while(!validGrade);
+			
+			Double points = gradeValue * credits;//Double.parseDouble(credits);
+		    
+			totalPoints += points;
+			totalCredits += credits;
+			
+			System.out.print("IF you like to enter another course? (Y/N)?");
+			String furtherCoursesString = scanner.nextLine();
+			
+			furtherCourses = furtherCoursesString.equalsIgnoreCase("Y");
 		}
-		while (!validCredits);//(validCredits == false);
+		while(furtherCourses);
 		
+		Double gpa = totalPoints / totalCredits;
+		String formGpa = String.format("%.2f", gpa);
 		
-		boolean validGrade = true;
-//		System.out.print("Enter grade            : ");
-//		String grade = scanner.nextLine();
-		String grade = "";
-		Double gradeValue = 0.0;
-		
-		do {
-		  validGrade = true;
-		  System.out.print("Enter grade: ");
-          grade = scanner.nextLine();
-		  
-		  if (grade.equalsIgnoreCase("A+") || (grade.equalsIgnoreCase("A"))) { //.equalsIgnoreCase-> for allow A,a without case sensitive
-			 gradeValue = 4.0;
-		  }else if (grade.equalsIgnoreCase("A-")) {
-			 gradeValue = 3.7;
-		  }else if (grade.equalsIgnoreCase("B+")) {
-			 gradeValue = 3.3;
-		  }else if (grade.equalsIgnoreCase("B")) {
-			 gradeValue = 3.0;
-		  }else if (grade.equalsIgnoreCase("B-")) {
-			 gradeValue = 2.7;
-		  }else if (grade.equalsIgnoreCase("C+")) {
-			 gradeValue = 2.4;
-		  }else if (grade.equalsIgnoreCase("C")) {
-			 gradeValue = 2.0;
-		  }else if (grade.equalsIgnoreCase("C-")) {
-			 gradeValue = 1.7;
-		  }else if (grade.equalsIgnoreCase("D+")) {
-			 gradeValue = 1.3;
-		  }else if (grade.equalsIgnoreCase("D")) {
-			 gradeValue = 1.0;
-		  }else if (grade.equalsIgnoreCase("E")) {
-			 gradeValue = 0.0;
-		  }else {
-			System.out.println("Grade is not Valid");		
-		    validGrade = false;
-		  }
-		}
-		while(!validGrade);
-		
-		
-		Double points = gradeValue * credits;//Double.parseDouble(credits);
-		Double gpa = points / credits;//Double.parseDouble(credits);
+		//Double points = gradeValue * credits;//Double.parseDouble(credits);
+		//Double gpa = points / credits;//Double.parseDouble(credits);
 		
 		System.out.println("\n");
-		System.out.println("Credits:" + credits);//creditsString
-		System.out.println("Grade  :" + grade);
-		System.out.println("Points :" + points);
+		System.out.println("Total number of Credits:" + totalCredits);//credits);//creditsString
+		//System.out.println("Grade  :" + grade);
+		System.out.println("Total number of Points :" + totalPoints); //points);
 		
-		System.out.println("GPA    :" +gpa);
+		System.out.println("Your GPA is   :" +formGpa);
 		
 		scanner.close();
 				
